ce23784369c6321339de95b58a0f0005b8ff426d
tk <takumi12430@yahoo.co.jp>
2020-05-14 12:07:33 +0900

    ステージの描画

--- stage.h
+++ stage.h
@@ -6,6 +6,14 @@
 
 //#pragma once//----------------------------------
 
+// 定数宣言
+#define MAP_Y_FIELD 20
+#define MAP_X_FIELD 30
+#define MAP_IMAGESIZE_X	32	// マップ画像のX方向のサイズ
+#define MAP_IMAGESIZE_Y	32	// マップ画像のY方向のサイズ
+
+//
+
 // プロトタイプ宣言	
 void StageGameDraw(void);	// ステージ関連の描画
 void StageSystemInit(void);	// ステージ関連の初期化

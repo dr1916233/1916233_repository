6c9ffbd0fb271537485f0bd4e80df4daa0b394d8
sirataki9 <yuta1015unmei@outlook.jp>
2020-05-14 15:14:56 +0900

    s

--- enemy.h
+++ enemy.h
@@ -2,6 +2,10 @@
 //	IsekaiKanchoProject
 //	By ゲヱム道館門下生
 //----------------------------------
+#define ENEMY_IMAGENUM_X 4	// エネミー画像のX方向の数
+#define ENEMY_IMAGENUM_Y 4	// エネミー画像のY方向の数
+#define ENEMY_IMAGESIZE_X	32	// エネミー画像のX方向のサイズ
+#define ENEMY_IMAGESIZE_Y	32	// エネミー画像のY方向のサイズ
 
 // 定数
 enum ENEMY_TYPE

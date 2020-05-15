a374c8bd15b8279036945d0fc52b40ee61e90e29
sirataki9 <yuta1015unmei@outlook.jp>
2020-05-15 12:41:39 +0900

    enemy関係を編集

--- enemy.cpp
+++ enemy.cpp
@@ -8,31 +8,23 @@
 #include "enemy.h"
 
 // •Ï”éŒ¾
-CHARACTER enemyMaster[ENEMY_TYPE_MAX];		// “G‚Ì•Ï”
+CHARACTER enemyMaster[ENEMY_TYPE_MAX];	// “G‚Ì•Ï”
 CHARACTER enemy[ENEMY_MAX];
-int enemyImage[ENEMY_TYPE_MAX][16];
+int enemyImage[ENEMY_TYPE_MAX][16];		// “G‚Ì‰æ‘œ‚Ìí—Ş
 
 // “G‚Ì‰Šú‰»
 void EnemySystemInit(void)
 {
-
-}
-
-// “G‚ÌƒQ[ƒ€‚²‚Æ‚Ì‰Šú‰»
-void EnemyGameInit(void)
-{
 	//------------------------------------------------------------------------------------------
 // ƒXƒ‰ƒCƒ€
 
-	enemyMaster[ENEMY_TYPE_SURAIMU].type = CHARA_ENEMY;
-	enemyMaster[ENEMY_TYPE_SURAIMU].lifeMax = 4;
-	enemyMaster[ENEMY_TYPE_SURAIMU].moveSpeed = 1;
-	enemyMaster[ENEMY_TYPE_SURAIMU].size.x = 32;
-	enemyMaster[ENEMY_TYPE_SURAIMU].size.y = 32;
+	//enemyMaster[ENEMY_TYPE_SURAIMU].type = CHARA_ENEMY;
+	//enemyMaster[ENEMY_TYPE_SURAIMU].lifeMax = 4;
+	//enemyMaster[ENEMY_TYPE_SURAIMU].moveSpeed = 1;
+	//enemyMaster[ENEMY_TYPE_SURAIMU].size.x = 32;
+	//enemyMaster[ENEMY_TYPE_SURAIMU].size.y = 32;
 	LoadDivGraph("image/character/ƒXƒ‰ƒCƒ€finŠ®.png",
 		16, 4, 4, 32, 32, enemyImage[ENEMY_TYPE_SURAIMU]);
-	
-
 	//------------------------------------------------------------------------------------------
 
 	for (int cnt = 0; cnt < ENEMY_TYPE_MAX; cnt++)
@@ -40,12 +32,20 @@ void EnemyGameInit(void)
 		enemyMaster[cnt].animCnt = 0;
 		enemyMaster[cnt].lifeMax = 0;
 		enemyMaster[cnt].life = 0;
-		enemyMaster[cnt].pos = { 0,0 };
-		enemyMaster[cnt].size.x = 0;
-		enemyMaster[cnt].size.y = 0;
-		enemyMaster[cnt].type = CHARA_ENEMY;
 		enemyMaster[cnt].dir = DIR_DOWN;
 		enemyMaster[cnt].visible = true;
+	}
+}
+
+// “G‚ÌƒQ[ƒ€‚²‚Æ‚Ì‰Šú‰»
+void EnemyGameInit(void)
+{
+	for (int en = 0; en < ENEMY_MAX; en++)
+	{
+		int rnd = GetRand(ENEMY_TYPE_MAX - 1);
+		enemy[en] = enemyMaster[rnd];
+		//enemy[cnt].pos.x = GetRand(SCREEN_SIZE_X - 1);
+		//enemy[cnt].pos.y = GetRand(SCREEN_SIZE_Y - 1);
 	}
 
 }
@@ -53,8 +53,20 @@ void EnemyGameInit(void)
 // “G‚Ì•`‰æ
 void EnemyGameDraw(void)
 {
-	enemy[ENEMY_MAX].animCnt++;
-	DrawGraph(enemy[ENEMY_MAX].pos.x, enemy[ENEMY_MAX].pos.y, enemyImage[enemy[ENEMY_MAX].dir][enemy[ENEMY_MAX].animCnt / 10 % 4], true);
+	enemy[ENEMY_TYPE_MAX].animCnt++;
+
+	int picMove = (enemy[ENEMY_TYPE_MAX].visible == false) ? enemy[ENEMY_TYPE_MAX].dir * 4 + enemy[ENEMY_TYPE_MAX].animCnt / 10 % 4 :
+		enemy[ENEMY_TYPE_MAX].dir * 4;
+
+	picMove = enemy[ENEMY_TYPE_MAX].dir * 4 + enemy[ENEMY_TYPE_MAX].animCnt / 10 % 4;
+
+	//for (int en = 0; en < ENEMY_MAX; en++)
+	//{
+		DrawGraph(enemy[ENEMY_TYPE_MAX].pos.x,
+			enemy[ENEMY_TYPE_MAX].pos.y,
+			enemyImage[enemy[ENEMY_TYPE_MAX].dir][picMove],
+			true);
+	//}
 
 	//DrawCircle(320, 240, 150, GetColor(255,255,255), false);
 
@@ -65,3 +77,4 @@ void EnemyControl(void)
 {
 	
 }
+	

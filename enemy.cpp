17ab63ad56e1c8245918cae0caa09961345e5c05
sirataki9 <yuta1015unmei@outlook.jp>
2020-05-18 13:53:45 +0900

    EnemyRadを追加

--- enemy.cpp
+++ enemy.cpp
@@ -9,7 +9,7 @@
 
 // 変数宣言
 CHARACTER enemyMaster[ENEMY_TYPE_MAX];	// 敵の変数
-CHARACTER enemy[ENEMY_MAX];
+CHARACTER enemy[ENEMY_MAX];				// 敵の総数
 int enemyImage[ENEMY_TYPE_MAX][16];		// 敵の画像の種類
 
 // 敵の初期化
@@ -17,26 +17,36 @@ void EnemySystemInit(void)
 {
 	for (int cnt = 0; cnt < ENEMY_TYPE_MAX; cnt++)
 	{
-		enemyMaster[cnt].size = { 0,0 };
-		enemyMaster[cnt].offsetSize = { 0,0 };
-		enemyMaster[cnt].animCnt = 0;
-		enemyMaster[cnt].lifeMax = 0;
-		enemyMaster[cnt].life = 0;
-		enemyMaster[cnt].dir = DIR_DOWN;
-		enemyMaster[cnt].visible = true;
+		enemyMaster[cnt].size = { 0,0 };		// 敵の初期サイズ
+		enemyMaster[cnt].offsetSize = { 0,0 };	// 敵の初期オフセットサイズ
+		enemyMaster[cnt].animCnt = 0;			// 敵の初期アニメーションカウント
+		enemyMaster[cnt].lifeMax = 0;			// 敵の初期最大ライフ
+		enemyMaster[cnt].life = 0;				// 敵の初期ライフ
+		enemyMaster[cnt].dir = DIR_DOWN;		// 敵の初期向き
+		enemyMaster[cnt].visible = true;		
 		enemyMaster[cnt].type = CHARA_ENEMY;
 	}
 
 	//------------------------------------------------------------------------------------------
 // スライム
 
-	enemyMaster[ENEMY_TYPE_SLIME].size = { 32,32 };
-	enemyMaster[ENEMY_TYPE_SLIME].offsetSize = { 3,7 };
-	enemyMaster[ENEMY_TYPE_SLIME].lifeMax = 2;
-	enemyMaster[ENEMY_TYPE_SLIME].moveSpeed = 1;
+	enemyMaster[ENEMY_TYPE_SLIME].size = { 32,32 };		// スライムのサイズ
+	enemyMaster[ENEMY_TYPE_SLIME].offsetSize = { 3,7 }; // スライムのオフセットサイズ
+	enemyMaster[ENEMY_TYPE_SLIME].lifeMax = 2;			// スライムのライフ
+	enemyMaster[ENEMY_TYPE_SLIME].moveSpeed = 1;		// スライムのスピード
 
 	LoadDivGraph("image/character/スライムfin完.png",
-		16, 4, 4, 32, 32, enemyImage[ENEMY_TYPE_SLIME]);
+		16, 4, 4, 32, 32, enemyImage[ENEMY_TYPE_SLIME]);	// スライムの描画
+	//------------------------------------------------------------------------------------------
+	// ゴブリン
+
+	enemyMaster[ENEMY_TYPE_GOBLIN].size = { 32,32 };	// ゴブリンのサイズ
+	enemyMaster[ENEMY_TYPE_GOBLIN].offsetSize = { 3,7 };// ゴブリンのオフセットサイズ
+	enemyMaster[ENEMY_TYPE_GOBLIN].lifeMax = 2;			// ゴブリンのライフ
+	enemyMaster[ENEMY_TYPE_GOBLIN].moveSpeed = 1;		// ゴブリンのスピード
+
+	LoadDivGraph("image/character/ゴブリンfin完.png",
+		16, 4, 4, 32, 32, enemyImage[ENEMY_TYPE_GOBLIN]);	// ゴブリンの描画
 	//------------------------------------------------------------------------------------------
 
 }
@@ -51,6 +61,7 @@ void EnemyGameInit(void)
 
 		//enemy[en].pos.x += enemy[en].size.x * en;
 
+		// スクリーンのランダムな位置で出現
 		enemy[en].pos.x = GetRand(SCREEN_SIZE_X - 1);
 		enemy[en].pos.y = GetRand(SCREEN_SIZE_Y - 1);
 	}
@@ -72,9 +83,9 @@ void EnemyGameDraw(XY mapPos)
 		DrawGraph(
 				  enemy[en].pos.x -mapPos.x,
 				  enemy[en].pos.y -mapPos.y,
-				  enemyImage[enemy[en].dir][picMove],
+				  enemyImage[enemy[ENEMY_TYPE_MAX].dir][picMove],
 		  		  true);
-	}
+	};
 
 	//DrawCircle(320, 240, 150, GetColor(255,255,255), false);
 
@@ -97,4 +108,10 @@ XY GetEnemySize(int en)
 
 	return enemy[en].size;
 }
+
+void EnemyRad(void)
+{
+	//17+26= 43/2= 21.5= 21
+}
+
 	
